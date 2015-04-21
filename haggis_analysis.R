662a46592a65fe4805ee71cedda3cf9e15948adf
susjoh <Susan.Johnston@ed.ac.uk>
2015-04-21 22:39:33 +0100

    Commit after knitr slides

--- haggis_analysis.R
+++ haggis_analysis.R
@@ -1,7 +1,7 @@
-### Haggis population density in the Scottish Highlands
-### S. Johnston, University of Edinburgh
+### Haggis population density in the Scottish Highlands.
+### S. Johnston, University of Edinburgh.
 
-# Load libraries and data files
+# Load libraries, set working directory, and load data files
 
 library(ggplot2)
 
@@ -17,14 +17,12 @@ fit1 <- lm(PopSize ~ Year, data = hds)
 fit1$coefficients
 
 slope <- fit1$coefficients[2]
-slope
 
 # What is the P-value?
 
 summary(fit1)$coefficients
 
 pval <- summary(fit1)$coefficients["Year", "Pr(>|t|)"]
-pval
 
 # Plot the regression
 
